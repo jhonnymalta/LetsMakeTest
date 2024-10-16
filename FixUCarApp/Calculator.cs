e178b88f6c94f79c3e40c616c280323e83ed59da
jhonny malta <jhonatanlandes@gmail.com>
2024-10-16 11:57:35 -0300

    criate test project nunit

---
+++ FixUCarApp/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixUCarApp
+{
+    public class Calculator
+    {
+        public int addNumbers(int a,int b)
+        {
+            return a + b;
+        }
+
+        public bool IsOddNumber(int a)
+        {
+            return (a % 2 == 0);
+        }
+    }
+}
---
+++ FixUCarApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FixUCarApp;
+
+Console.WriteLine("Welcome to calculator FixUCar");
+
+var calculator = new Calculator();
+var soma = calculator.addNumbers(2, 50);
+
+Console.WriteLine(soma);
+Console.ReadKey();
